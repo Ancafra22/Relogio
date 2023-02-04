3bdeaab68f361f7069e6e9f3411fd38c5399cfb2
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2023-02-04 17:14:25 -0300

    Project creation. Component mapping. Layout creation.

--- app/src/main/java/com/ancafra/relogio/MainActivity.java
+++ app/src/main/java/com/ancafra/relogio/MainActivity.java
@@ -3,12 +3,28 @@ package com.ancafra.relogio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+    public static class ViewHolder{
+        TextView textHourMinute;
+        TextView textSecond;
+        TextView textBattery;
+    }
+
+    private void initComponents() {
+        this.mViewHolder.textHourMinute = findViewById(R.id.text_hour_minute);
+        this.mViewHolder.textSecond = findViewById(R.id.text_seconds);
+        this.mViewHolder.textBattery = findViewById(R.id.text_battery);
     }
 }
