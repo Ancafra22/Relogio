efe616b8050402168b47e6c045843e69cb4b3794
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2023-02-04 17:46:26 -0300

    Creation of the method tha the show battery level in the app.

--- app/src/main/java/com/ancafra/relogio/MainActivity.java
+++ app/src/main/java/com/ancafra/relogio/MainActivity.java
@@ -2,12 +2,25 @@ package com.ancafra.relogio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mViewHolder.textBattery.setText(String.format("%d%%", level));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +39,7 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textHourMinute = findViewById(R.id.text_hour_minute);
         this.mViewHolder.textSecond = findViewById(R.id.text_seconds);
         this.mViewHolder.textBattery = findViewById(R.id.text_battery);
+
+        this.registerReceiver(this.mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 }
