feb95bc140d90f8d36f4171e61a1d3c353e4c09e
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2023-02-08 14:01:46 -0300

    setting nocturne mode

--- app/src/main/java/com/ancafra/relogio/MainActivity.java
+++ app/src/main/java/com/ancafra/relogio/MainActivity.java
@@ -75,6 +75,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void showSystemUi() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+
     private void startClock() {
 
         Calendar calendar = Calendar.getInstance();
@@ -102,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                         mViewHolder.textNight.setText("Good Morning");
                     } else if (hour > 12 && hour <= 18) {
                         mViewHolder.textNight.setText("Good Afternoon");
-                    }else{
+                    } else {
                         mViewHolder.textNight.setText("Good Night");
                     }
                 }
@@ -131,6 +139,13 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textSecond = findViewById(R.id.text_seconds);
         this.mViewHolder.textBattery = findViewById(R.id.text_battery);
         this.mViewHolder.textNight = findViewById(R.id.textNight);
+
+        this.mViewHolder.textHourMinute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSystemUi();
+            }
+        });
 
     }
 }
